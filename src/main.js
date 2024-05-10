4e8014f393b2fff5f76b3fad2d041d649eda7bbf
DaanHessen <daanh2002@gmail.com>
2024-05-10 14:31:14 +0200

    navbar changes

--- src/main.js
+++ src/main.js
@@ -1,30 +1,46 @@
 import './view/pages/home-page';
 
 // menu for smaller devices
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+const closeButton = document.getElementById('nav-close'); // Update closeButton to refer to the hamburger menu close button
 
 navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu')
-})
+    if (window.innerWidth <= 1023) {
+        navMenu.classList.add('show-menu');
+        toggleNavClose();
+    }
+});
 
 navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu')
-})
+    navMenu.classList.remove('show-menu');
+    toggleNavClose();
+});
 
-
+function toggleNavClose() {
+    navClose.classList.toggle('visible');
+    closeButton.classList.toggle('visible'); // Toggle the visibility of the close button
+}
 
 // login
-const loginBtn = document.getElementById('login-btn'),
-      loginClose = document.getElementById('login-close'),
-      loginForm = document.getElementById('login'),
-      body = document.body;
+const loginBtn = document.getElementById('login-btn');
+const loginClose = document.getElementById('login-close');
+const loginForm = document.getElementById('login');
+const body = document.body;
 
-      loginBtn.addEventListener('click', toggleLogin);
-      loginClose.addEventListener('click', toggleLogin);
-      
-      function toggleLogin() {
-          loginForm.classList.toggle('show');
-          body.classList.toggle('show-login-overlay');
-      }
+loginBtn.addEventListener('click', () => {
+    toggleLogin();
+    toggleNavClose();
+});
+
+loginClose.addEventListener('click', () => {
+    toggleLogin();
+    toggleNavClose();
+});
+
+function toggleLogin() {
+    loginForm.classList.toggle('show');
+    body.classList.toggle('show-login-overlay');
+    closeButton.classList.toggle('visible'); // Toggle the visibility of the close button
+}
